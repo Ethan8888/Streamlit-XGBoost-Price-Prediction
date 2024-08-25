29c968560a688806cc4d6d9d04b70cd2b74bd473
Ethan8888 <64136291+Ethan8888@users.noreply.github.com>
2024-08-26 00:04:37 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,15 +6,9 @@ import time
 import os
 
 # Everything is accessible via the st.secrets dict:
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-# And the root-level secrets are also accessible as environment variables:
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
+st.write("DB username:", st.secrets["Ethan"])
+st.write("DB password:", st.secrets["12345"])
+st.write("My cool secrets:", st.secrets["Streamlit"]["Python"])
 
 st.set_page_config(
    page_title="Predictor",
