7ed734c1f3619948c1a3586acb5c47f8bb1b242f
Ethan8888 <64136291+Ethan8888@users.noreply.github.com>
2024-08-25 18:07:24 +0800

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,72 @@
+import numpy as np 
+import pandas as pd 
+import pickle 
+import streamlit as st
+import xgboost
+from xgboost import XGBRegressor 
+
+st.set_page_config(
+   page_title=" Prediction",
+   layout="wide",
+   initial_sidebar_state="expanded")
+
+# Load the trained machine learning model
+    model = XGBRegressor ()
+    model.load_model("Prediction")
+
+def predict(Open,Close,High,Low):
+    input_data = np.array([[Open,Close,High,Low]])
+    return model.predict(input_data)[0]
+
+
+def main():
+    code_snippet = """
+        R-squared (R2): 0.78
+        """
+    st.sidebar.code(code_snippet, language='python')
+
+    # Title of The WebPage
+    st.title('Machine Learning Prediction')
+    st.warning("warning")
+
+    #Taking Inputs From The User....
+    car = st.form('my_car') 
+    sub = car.form_submit_button('Predict')
+    Open = car.number_input("Enter:",max_value=4201.4, min_value=2211.8, step=1.0)
+    Close = car.number_input('Enter:',max_value=4212.9, min_value=2212.7, step=1.0)
+    High = car.number_input('Enter:',max_value=4244.7, min_value=2223.2, step=1.0)
+    Low = car.number_input('Enter:',max_value=4173.0, min_value=2203.4, step=1.0)
+    result = predict(Open,Close,High,Low)
+    sub = car.form_submit_button('Predict')
+
+    if sub:
+        with st.spinner(f'Processing Wait a sec...{name} '):
+            time.sleep(10)
+
+
+        st.divider()
+        
+        #st.error(f'Good to See You {name}')
+        
+        st.subheader("Prediction Report")
+        
+        code_snippet = f"""
+        Good To See You {name}
+        Open:   {Open}
+        Close :   {Close}
+        High:   {High}
+        Low:   {Low}
+        
+        Result: 
+
+        Value -->  {result} """
+        
+        st.code(code_snippet, language='python')
+
+
+        
+
+
+    footer()
+if __name__ == '__main__':
+    main()
