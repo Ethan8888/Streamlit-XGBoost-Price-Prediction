b23d615634ee803cb65ba1ff9d7d382243612dd5
Ethan8888 <64136291+Ethan8888@users.noreply.github.com>
2024-08-25 15:35:19 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,11 +15,11 @@ st.set_page_config(
 
 # Load the trained machine learning model
 with open('Prediction.pkl', 'rb') as f:
-    reg = pickle.load(f)
+    xgb = pickle.load
 
 def predict(Open,Close,High,Low):
     input_data = np.array([[Open,Close,High,Low]])
-    return reg.predict(input_data)[0]
+    return xgb.predict(input_data)[0]
 
 
 def main():
